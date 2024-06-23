43c5f82d66a37f76a58254bb9cf9cc60b9129335
Victor <vatorresm88@gmail.com>
2024-06-22 22:11:21 -0500

    Optimizando codigo

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,57 +2,44 @@ import { Router } from 'express';
 import { body, param } from 'express-validator';
 import { AuthController } from '../controllers/AuthController';
 import { handleInputErrors } from '../middleware/validation';
-import { TaskController } from '../controllers/TaskController';
-import { validateUserExists } from '../middleware/user';
 
 const routerAuth = Router();
 
+// Common validators
+const userNameValidator = body('userName').notEmpty().withMessage('El Nombre del Usuario es Obligatorio');
+const userEmailValidator = body('userEmail').isEmail().withMessage('Email no válido');
+const userPasswordValidator = body('userPassword')
+  .isLength({ min: 8 })
+  .withMessage('El password es muy corto, minimo 8 caracteres');
+const passwordConfirmationValidator = body('password_confirmation').custom((value, { req }) => {
+  if (value !== req.body.userPassword) {
+    throw new Error('Los Password no son iguales');
+  }
+  return true;
+});
+const userIdParamValidator = param('id').isMongoId().withMessage('El ID de usuario no es válido');
+
+// Create account route
 routerAuth.post(
   '/create-account',
-  body('userName')
-    .notEmpty()
-    .withMessage('El Nombre del Usuario es Obligatorio'),
-  body('userEmail').isEmail().withMessage('Email no válido'),
-  body('userPassword')
-    .isLength({ min: 8 })
-    .withMessage('El password es muy corto, minimo 8 caracteres'),
-  body('password_confirmation').custom((value, { req }) => {
-    if (value !== req.body.userPassword) {
-      throw new Error('Los Password no son iguales');
-    }
-    return true;
-  }),
-  handleInputErrors,
+  [userNameValidator, userEmailValidator, userPasswordValidator, passwordConfirmationValidator, handleInputErrors],
   AuthController.createAccount
 );
 
+// Get all users route
 routerAuth.get('/', AuthController.getAllUsers);
 
-routerAuth.get(
-  '/:id',
-  param('id').isMongoId().withMessage('El ID de usuario no es válido'),
-  handleInputErrors,
-  AuthController.getUserById
-);
+// Get user by ID route
+routerAuth.get('/:id', [userIdParamValidator, handleInputErrors], AuthController.getUserById);
 
+// Update user route
 routerAuth.put(
   '/:id',
-  body('userName')
-    .notEmpty()
-    .withMessage('El Nombre del Usuario es Obligatorio'),
-  body('userEmail').notEmpty().withMessage('El Email  es Obligatorio'),
-  body('userPassword')
-    .isLength({ min: 8 })
-    .withMessage('El password es muy corto, minimo 8 caracteres'),
-  handleInputErrors,
+  [userNameValidator, userEmailValidator.notEmpty().withMessage('El Email es Obligatorio'), userPasswordValidator, handleInputErrors],
   AuthController.updateUser
 );
 
-routerAuth.delete(
-  '/:id',
-  param('id').isMongoId().withMessage('El ID del Usuario no es válido'),
-  handleInputErrors,
-  AuthController.deleteUser
-);
+// Delete user route
+routerAuth.delete('/:id', [userIdParamValidator, handleInputErrors], AuthController.deleteUser);
 
 export default routerAuth;
